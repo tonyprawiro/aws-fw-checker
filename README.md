9c4da6cd834b1278c088d401f0d05ecb2d487b0d
Tony Hadimulyono <tony_hadimulyono@ida-gds.com>
2017-01-19 12:31:41 +0800

    More documentation, optimised some loops

--- README.md
+++ README.md
@@ -1,3 +1,34 @@
 # aws-fw-checker
 
-A simple script to perform a firewall check against your own (AWS) machines using external cloud provider.
+A simple script to perform a firewall check against your own (AWS) machines by scanning security group rules.
+
+## How to run
+
+```
+$ AWS_ACCESS_KEY_ID=AKIA123123123123123123123 \
+  AWS_SECRET_ACCESS_KEY="1232341231233423132353452412134235245243" \
+  AWS_REGION_NAME="ap-southeast-1" \
+  TELEGRAM_GDS_CHATOPS_BOT_TOKEN="123123123:abcdef123123123123123123123123123123" \
+  TELEGRAM_FWCHECK_CHATGROUP_ID="-123123123123" \
+  python check.py
+```
+
+Example output:
+
+```
+~Daily EC2 SecGroup Sanity Check~
+The following EC2 instances are running, have public IP addresses, and are publicly accessible (0.0.0.0/0) 
+at the following port(s). Please review and ensure appropriate access control is in place. 
+
+Name: My Machine Name (i-abc123def)
+IP address: 54.123.123.123
+Port(s): 80, 443
+
+To check the exclusion list, or add machine:port into the list, please inform DevOps team.
+```
+
+# Improvements
+
+- Need to check ELB and RDS, too
+
+- 
--- check.py
+++ check.py
@@ -16,10 +16,21 @@ def get_instance_tag_value(arr, tag_name = "Name"):
     return result
 
 # Load exclusion list, exclude.json, which must be in the same directory as this file
+# example:
+# {
+#     "i-abc12345": [
+#         "80",
+#         "443"
+#     ],
+#     "i-def54321": [
+#         "8443"
+#     ]
+# }
 exclusion_json = ""
 with open("%s/exclude.json" % (os.path.dirname(os.path.realpath(__file__)))) as f:
     exclusion_json = f.read()
 exclusions = json.loads(exclusion_json)
+
 
 # Create boto3 EC2 client
 ec2 = boto3.client('ec2',
@@ -27,10 +38,11 @@ ec2 = boto3.client('ec2',
                    aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
                    region_name=os.environ["AWS_REGION_NAME"])
 
-# Get security groups and cache group/ports information
+
+# Get security groups from EC2, and then cache group/ports information for easy lookup later
 secgroups = ec2.describe_security_groups()
-secgroups_scrutiny = []
-secgroups_public_ports = {}
+secgroups_scrutiny = []  # Security group IDs that contains 0.0.0.0/0
+secgroups_public_ports = {}  # Ports
 for secgroup in secgroups["SecurityGroups"]:
     calc = []
     for ingress in secgroup["IpPermissions"]:
@@ -49,17 +61,24 @@ for secgroup in secgroups["SecurityGroups"]:
                     secgroups_scrutiny.append(secgroup["GroupId"])
     secgroups_public_ports[secgroup["GroupId"]] = calc
 
+
 # Get machines
-instances = ec2.describe_instances(MaxResults=1000)
+instances = ec2.describe_instances(MaxResults=1000)  # Get all machines
 machines = []
 for res in instances["Reservations"]:
+
     for inst in res["Instances"]:
+
+        """inst is a single instance"""
         public_ip = ""
         try:
             public_ip = inst["PublicIpAddress"]
         except:
             pass
-        if public_ip != "" and inst["State"]["Code"] == 16:
+
+        if public_ip != "" and inst["State"]["Code"] == 16:  # Only process if machine is running with public IP
+
+            # Gather basic information about the machine
             machine = {}
             machine["State"] = inst["State"]["Name"]
             machine["InstanceId"] = inst["InstanceId"]
@@ -68,39 +87,50 @@ for res in instances["Reservations"]:
                 machine["Name"] = inst["InstanceId"]
             machine["IpAddress"] = public_ip
             machine["Ports"] = []
+
+            # Iterate all security groups associated with the machine
             for grp in inst["SecurityGroups"]:
-                if grp["GroupId"] in secgroups_scrutiny:
-                    for secgroup in secgroups["SecurityGroups"]:
-                        if secgroup["GroupId"] == grp["GroupId"]:
-                            for ps in secgroups_public_ports[grp["GroupId"]]:
-                                if ps not in machine["Ports"]:  # not already added
-                                    try:
-                                        if ps not in exclusions[inst["InstanceId"]]:
-                                            machine["Ports"].append(ps)
-                                    except:
-                                        machine["Ports"].append(ps)
-            if len(machine["Ports"]) > 0:
+
+                if grp["GroupId"] in secgroups_scrutiny:  # If the security group contains 0.0.0.0/0 (in scrutiny)
+
+                    for ps in secgroups_public_ports[grp["GroupId"]]:  # Check the ports in this security group
+
+                        if ps not in machine["Ports"]:  # If this port has not been noted before
+
+                            # If there is exclusion for this machine, and the port is not in the list,
+                            # then note this port
+                            # If there is no exclusion for this machine, then note this port
+                            try:
+                                if ps not in exclusions[inst["InstanceId"]]:
+                                    machine["Ports"].append(ps)
+                            except:
+                                machine["Ports"].append(ps)
+
+            if len(machine["Ports"]) > 0:  # There are some exposed ports and not in exclusion list
                 machines.append(machine)
 
-# Generate the output
-output = "_~Daily EC2 SecGroup Sanity Check~_\n"
+
+# Generate the output, fancy stuff..
+output = "_~Daily EC2 SecGroup Sanity Check~_\n\n"
 if len(machines)>0:
     output += "The following EC2 instances are running, have public IP addresses, "
     output += "and are publicly accessible (0.0.0.0/0) at the following port(s). "
     output += "Please review and ensure appropriate access control is in place. "
-    output += ""
-    output += "\n"
+    output += "\n\n"
     for machine in machines:
         output += "Name: %s (%s)\n" % (machine["Name"], machine["InstanceId"])
         output += "IP address: %s\n" % machine["IpAddress"]
         output += "Port(s): %s\n" % (", ".join(machine["Ports"]))
         output += "\n"
 else:
-    output += "\nEverything seems to be in order. See you tomorrow~\n"
+    output += "Everything seems to be in order. See you tomorrow~\n"
 
-output += "\nTo check the exclusion list, or add machine:port into the list, please inform @tonyhadimulyono.\n"
+output += "\nTo check the exclusion list, or add machine:port into the list, please inform DevOps team.\n"
 
+
+# Send result to stdin
 print output
+
 
 # Send result via Telegram
 print "Sending result via Telegram... "
@@ -114,3 +144,4 @@ try:
     print "done!"
 except:
     print "failed!"
+
